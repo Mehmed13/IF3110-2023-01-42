40137c55ef62d3a882bc34295c6ec9d8d598c075
fadhilAmri <fadhil133113@gmail.com>
2023-10-03 06:07:11 +0700

    feat: add profile picture request to server

--- scripts/clientside/pages/profile/profile.js
+++ scripts/clientside/pages/profile/profile.js
@@ -1,7 +1,41 @@
-var loadFile = function (event) {
-    var image = document.getElementById("output");
-    var profileIcon = document.getElementById("profileIcon");
+const DATA_PATH = "../../../../../Data"
+const PROFILE_PICTURE_PATH = DATA_PATH + "/profilePicture"
+
+var profilePictureURL = PROFILE_PICTURE_PATH+"/";
+var defaultProfilePictureURL = "../../assets/albert.jpg";
+var inputFile = document.getElementById("profilePhotoFile");
+var profileimage = document.getElementById("output");
+var profileIcon = document.getElementById("profileIcon");
+
+// Dummy, nantinya bakal disesuaiin ama user yg login 
+var profileId = 1;
+
+function loadFile(event) {
     image.src = URL.createObjectURL(event.target.files[0]);
     profileIcon.src = URL.createObjectURL(event.target.files[0]);
-  };
-  
+};
+
+function loadProfile(){
+  getProfilePicture();
+  image.src = profilePictureURL;
+}
+
+function getProfilePicture(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            var profileFile = JSON.parse(this.responseText);
+            if(profileFile['status']){
+                profilePictureURL+= profileFile['data'];
+            }else{
+                profilePictureURL = defaultProfilePictureURL;
+            }
+        }
+    };
+    xhttp.open("GET","http://localhost:8000/api/profileapi/getprofile?profile_id="+profileId,true);
+    // xhttp.setRequestHeader("Accept", "application/json");
+    xhttp.withCredentials = true;
+    xhttp.send();
+}
+inputFile.addEventListener("change", loadFile);
+image.addEventListener("load", )
