e081fbe5e0225f25ab9387d74c2ef48fcba22b40
fadhilAmri <fadhil133113@gmail.com>
2023-10-05 13:28:07 +0700

    feat: add edit profile service

--- scripts/serverside/app/models/User.php
+++ scripts/serverside/app/models/User.php
@@ -21,4 +21,23 @@ class User
             return  false;
         }   
     }
+
+    public function editProfile($ID_Pengguna, $nama_depan, $nama_belakang, $username, $email, $password, $profile_pict){
+        $this->db->query("UPDATE " . $this->table . " SET nama_depan = :nama_depan, nama_belakang = :nama_belakang, username = :username
+            email = :email, password = :password, profile_pict = :profile_pict WHERE ID_Pengguna = :ID_Pengguna" );
+
+        $this->db->bindParam(':ID_Pengguna', $ID_Pengguna);
+        $this->db->bindParam(':nama_depan', $nama_depan);
+        $this->db->bindParam(':nama_belakang', $nama_belakang);
+        $this->db->bindParam(':username', $username);
+        $this->db->bindParam(':email', $email);
+        $this->db->bindParam(':password', $password);
+        $this->db->bindParam(':profile_pict', $profile_pict);
+        try {
+            $this->db->execute();
+            return true;
+        } catch (PDOException $e) {
+            return  false;
+        }
+    }
 }
