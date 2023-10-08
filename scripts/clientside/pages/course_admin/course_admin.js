956e1b9d4b48f83d9544fdff868d32c68c2b6a4f
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-10-08 17:49:43 +0700

    feat: course admin be not done

--- scripts/clientside/pages/course_admin/course_admin.js
+++ scripts/clientside/pages/course_admin/course_admin.js
@@ -1,42 +1,145 @@
-courseData = [
-  {
-    title: "Course 1",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    title: "Course 2",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    title: "Course 3",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-];
+let course_id = window.location.search.substring(1).split('=')[1];
+let parentDiv = document.getElementsByClassName("course");
+let addCourseForm = document.getElementsByClassName("addCourseForm")[0];
+let addCourseButton = document.getElementById("addCourseButton");
 
-var parentDiv = document.getElementById("course");
-courseData.map((el) =>
-  parentDiv.insertAdjacentHTML(
-    "beforeend",
-    `
-    <div class="course">
-                    <img 
-                    src="../../../../assets/module-profile.png" 
-                    alt="module profile icon"
-                    id="module-profile"
-                    />
-                    <div class="content">
-                        <h2>${el.title}</h2>
-                        <p>${el.desc}</p>
-                        <button>
-                            <h3>Edit</h3>
-                        </button>
-                        <button>
-                            <h3>Delete</h3>
-                        </button>
-                    </div>
-                </div>`
-  )
-);
+function loadCourse(courseData){
+  courseData.map((el) =>
+    parentDiv.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="content">
+          <div class="contentTitle">
+              <h2>${el.judul}</h2>
+          </div>
+  
+          <div class="adminButtons">
+              <button id="edit" onclick="editModule(${el.no_modul})">
+                  <h3>Edit</h3>
+              </button>
+              <button id="delete" onclick="deleteModule(${el.no_modul})">
+                  <h3>Delete</h3>
+              </button>
+          </div>
+          </div>
+          `
+          )
+          );
+        }
+        
+function loadAddCourseForm(){
+  addCourseForm.style.display = "flex";
+  
+  // When form appear, add event listener to submit the new course
+  let saveAddCourseButton = document.getElementsByClassName("saveButton")[0];
+  
+  // Add variable for input
+  let courseNumberInput = document.getElementById("courseNumber");
+  let courseTitleInput = document.getElementById("courseTitle");
+  let courseDescriptionInput = document.getElementById("courseDescription");
+  
+  saveAddCourseButton.addEventListener("click", function(){addCourse(courseNumberInput.value,  moduleTitleInput.value, 
+    courseDescriptionInput.value)});  
+}
+
+/* Connections to Server */
+function getCourses(){
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let courseData;
+      console.log(this.responseText);
+      let serverResponse = JSON.parse(this.responseText);
+      if (serverResponse["status"]) {
+        courseData = serverResponse["data"];
+      } else {
+        courseData = null;
+      }
+      loadCourse(courseData);
+    }
+  };
+  xhttp.open(
+    "GET",
+    "http://localhost:8000/api/moduleapi/getmodulebykodemapel?kode_mapel=" + course_id,
+    true
+  );
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.withCredentials = true;
+  xhttp.send();
+}
+
+function addCourse(no_course, judul, deskripsi){
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      let serverResponse = JSON.parse(this.responseText);
+      if(serverResponse['status']){
+        if(window.confirm("Course sucessfully Added")){
+            window.location.reload();
+        }else{
+            window.location.reload();
+        }
+      }
+      else{
+          alert("Failed to add course");
+      }
+    }
+  };
+
+  let data = {
+    no_course: no_course,
+    judul: judul,
+    deskripsi: deskripsi
+  };
+
+  xhttp.open("POST", "http://localhost:8000/api/moduleapi/addcourse", true);
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.withCredentials = true;
+  xhttp.send(JSON.stringify(data));
+}
+
+
+
+function deleteCourse(no_modul){
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      let serverResponse = JSON.parse(this.responseText);
+      if(serverResponse['status']){
+        if(window.confirm("Course sucessfully deleted")){
+            window.location.reload();
+        }else{
+            window.location.reload();
+        }
+      }
+      else{
+        alert("failed to delete course");
+      }
+    }
+  };
+
+  let data = {
+    no_modul:no_modul
+  };
+  xhttp.open("POST", "http://localhost:8000/api/moduleapi/deletemodule", true);
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.withCredentials = true;
+  xhttp.send(JSON.stringify(data));
+  
+}
+
+
+/* Redirect */
+function editCourse(no_course){
+  window.location.href= '../material_admin/material_admin.html?no_course=' + no_course};
+
+/* caller */
+window.addEventListener("load", getCourses);
+addCourseButton.addEventListener("click", loadAddCourseForm); 
 
 function loadPage() {
   generateNavbar();
