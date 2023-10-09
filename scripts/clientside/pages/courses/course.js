6f8ebdb74958d4933332496bacad5377bc224650
fadhilAmri <fadhil133113@gmail.com>
2023-10-09 10:13:57 +0700

    feat: add initial course_BE

--- scripts/clientside/pages/courses/course.js
+++ scripts/clientside/pages/courses/course.js
@@ -1,41 +1,65 @@
-courseData = [
-  {
-    title: "Course 1",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    title: "Course 2",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    title: "Course 3",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-];
-
-var parentDiv = document.getElementById("course");
-courseData.map((el) =>
-  parentDiv.insertAdjacentHTML(
-    "beforeend",
-    `
-    <a href="" class="course-link">
-        <div class="course">
-            <img 
-            src="../../../../assets/module-profile.png" 
-            alt="module profile icon"
-            id="module-profile"
-            />
-            <div class="content">
-                <h2>${el.title}</h2>
-                <p>${el.desc}</p>
-            </div>
-        </div>
-    </a>`
-  )
-);
+/* Variable and initialization */
+// Format get params.   ?kode_mapel=x&nama_mapel=y
+let parentDiv = document.getElementsByClassName("course")[0];
 
 function loadPage() {
-  auth(["admin", "user"], `/pages/home/home.html`);
+  auth(["user"], `/pages/home/home.html`);
   generateNavbar();
   generateFooter();
 }
+
+/* Functions */
+/* Display */
+function loadCourse(courseData) {
+  courseData.map((el) =>
+    parentDiv.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="content" onclick="openCourse(${el.kode_mapel}, ${el.judul})">
+          <img 
+          src="../../../../assets/module-profile.png" 
+          alt="module profile icon"
+          id="module-profile"
+          />
+          <div class="contentText">
+              <h2>${el.judul}</h2>
+              <p>${el.deskripsi}</p>
+          </div>
+      </div>
+      `
+    )
+  );
+}
+
+/* Connections to Server */
+function getCourses() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let courseData;
+      console.log(this.responseText);
+      let serverResponse = JSON.parse(this.responseText);
+      if (serverResponse["status"]) {
+        courseData = serverResponse["data"];
+      } else {
+        courseData = null;
+      }
+      loadCourse(courseData);
+    }
+  };
+  xhttp.open(
+    "GET",
+    "http://localhost:8000/api/courseapi/getallcourse",
+    true
+  );
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.withCredentials = true;
+  xhttp.send();
+}
+
+/* Redirect */
+function openCourse(kode_mapel, nama_mapel){
+  window.location.href= '../module/module.html?kode_mapel='+kode_mapel+" &nama_mapel=" + nama_mapel};
+
+/* caller */
+window.addEventListener("load", getCourses);
